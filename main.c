92ad5ad53180696147ab2169ad588667b4008df1
Eduardo Martins <eduardo.hmartins93@gmail.com>
2017-03-26 17:50:12 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	int A, B, C, D;
+	scanf("%d %d", &A, &B);
+	scanf("%d %d", &C, &D);
+	int DIFERENCA = A * B - C * D;
+	printf("DIFERENCA = %d\n",DIFERENCA);
+
+return 0;
+		
+}
